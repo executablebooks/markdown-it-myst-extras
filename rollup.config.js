69bda82d083d704dfb97bedbccf0aeeb5cc3375d
Rowan Cockett <rowanc1@gmail.com>
2022-02-14 15:30:51 -0700

    📦  Improve packaging export (#1)
    This is a downstream mirror of https://github.com/executablebooks/markdown-it-plugin-template/pull/11,
    that addresses many of the challenges in importing into an ESM native environment.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
-import { terser } from "rollup-plugin-terser"
 import typescript from "rollup-plugin-typescript2"
-import { babel } from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
-import { nodeResolve } from "@rollup/plugin-node-resolve"
-import json from "@rollup/plugin-json"
-
-import * as packageJson from "./package.json"
+import babel from "@rollup/plugin-babel"
+import resolve from "@rollup/plugin-node-resolve"
+import { terser } from "rollup-plugin-terser"
 
 export default {
   input: "src/index.ts",
@@ -13,20 +10,31 @@ export default {
     typescript(), // Integration between Rollup and Typescript
     commonjs(), // Convert CommonJS modules to ES6
     babel({ babelHelpers: "bundled" }), // transpile ES6/7 code
-    nodeResolve(), // resolve third party modules in node_modules
-    terser(), // minify generated bundle
-    json()  // import json files as modules
+    resolve() // resolve third party modules in node_modules
   ],
   output: [
     {
-      file: packageJson.main,
+      file: "dist/index.umd.js",
       format: "umd", // commonJS
       name: "markdownitMystExtra", // window.name if script loaded directly in browser
       sourcemap: true
     },
     {
-      file: packageJson.module,
+      file: "dist/index.umd.min.js",
+      format: "umd",
+      name: "markdownitMystExtra",
+      plugins: [terser()],
+      sourcemap: true
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true
+    },
+    {
+      file: "dist/index.esm.min.js",
       format: "esm", // ES Modules
+      plugins: [terser()],
       sourcemap: true
     }
   ]
